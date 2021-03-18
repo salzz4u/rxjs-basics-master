8e3c3c9aa5022c67279b01f746a1e3c625e9c104
salim kapasi <sgkapasi@gmail.com>
2021-03-18 17:28:04 -0400

    sampleTime example on clickEvent

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import {fromEvent, interval} from 'rxjs';
-import {sample, takeUntil, tap} from "rxjs/operators";
+import {fromEvent} from 'rxjs';
+import {map, sampleTime} from "rxjs/operators";
 import {subFunc} from "./helper";
 
 const source$ = fromEvent(document, 'click');
-const stop$ = fromEvent(document, 'keyup');
-const interval$ = interval(1000);
 
-interval$.pipe(
-    tap(e=>console.log('...' + e)),
-    sample(source$),
-    takeUntil(stop$)
+source$.pipe(
+    sampleTime(4000),
+    map(({clientX, clientY}) => ({
+        clientX, clientY
+    }))
 ).subscribe(subFunc('clicked'))
 
