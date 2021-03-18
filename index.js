be163b6e87159188c56a84a2ab3e995b11c577e9
salim kapasi <sgkapasi@gmail.com>
2021-03-18 16:14:48 -0400

    distinctUntilChanged example using array emits

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-import {fromEvent, timer} from 'rxjs';
-import {filter, scan, shareReplay, take, takeUntil} from "rxjs/operators";
+import {interval} from 'rxjs';
+import {distinctUntilChanged, map, take} from "rxjs/operators";
 
-const countDownNumber = 10;
-const speed = 1000;
-
-const scanFunc = (x, y) => {
-    return countDownNumber - y
-}
 const subFunc = (msg, next = true, complete = true) => {
     return {
         next: value => next ? console.log(value) : null,
         complete: () => complete ? console.log('completed: ' + msg) : null
     }
 }
-// click stream
-const click$ = fromEvent(document, 'click');
 
-const source$ = timer(0, speed).pipe(
-    take(countDownNumber + 1),
-    takeUntil(click$),
-    scan(scanFunc, countDownNumber),
-    // tap(() => console.log('...')),
-    shareReplay());
+const array = [1, 2, 2, 3, 3, '3', 4, 5];
 
+const delayedArrayItem$ = interval(500)
+    .pipe(
+        take(array.length),
+        map(i => array[i]),
+        distinctUntilChanged()
+    );
 
-const countDown$ = source$.pipe(filter(x => x > 0));
-const takeOff$ = source$.pipe(filter(x => x === 0));
-const mid$ = source$.pipe(filter(x => x === Math.round(countDownNumber / 2)), take(1));
+delayedArrayItem$.subscribe(subFunc('array'))
 
-countDown$.subscribe(subFunc('countDown'));
-takeOff$.subscribe(subFunc('takeOff!', null));
-mid$.subscribe(subFunc('halfway!', null));
 
