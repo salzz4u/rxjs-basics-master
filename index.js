5c7d4e2d6bd2178bf70ae24c1810c78d78961a37
salim kapasi <sgkapasi@gmail.com>
2021-03-18 15:45:39 -0400

    added subscription function that consoles next and complete conditionally

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ const countDownNumber = 15;
 const scanFunc = (x, y) => {
     return countDownNumber - y
 }
+const subFunc = (msg, next= true, complete = true) => {
+    return {
+        next: value => next ? console.log(value): null,
+        complete: () => complete?  console.log('completed: ' + msg) : null
+    }
+}
+
 const source$ = interval(1000).pipe(
     take(countDownNumber + 1),
     scan(scanFunc, countDownNumber),
-    tap(() => console.log('...')),
+    // tap(() => console.log('...')),
     shareReplay());
 
 const countDown$ = source$.pipe(filter(x => x > 0));
 const takeOff$ = source$.pipe(filter(x => x === 0));
-const mid$ = source$.pipe(filter(x => x === Math.round(countDownNumber / 2)));
+const mid$ = source$.pipe( filter(x => x === Math.round(countDownNumber / 2)),take(1));
 
-countDown$.subscribe(console.log);
-takeOff$.subscribe(() => console.log('takeoff!'));
-mid$.subscribe(() => console.log('halfway!'));
+countDown$.subscribe(subFunc('countDown'));
+takeOff$.subscribe(subFunc('takeOff!', null));
+mid$.subscribe(subFunc('halfway!', null));
 
