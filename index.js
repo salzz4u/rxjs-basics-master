5f9952deaf88019093d8df6f3e02dc93c2f0e6dd
salim kapasi <sgkapasi@gmail.com>
2021-03-18 15:59:26 -0400

    added abort using takeUntil

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {timer} from 'rxjs';
-import {filter, scan, shareReplay, take} from "rxjs/operators";
+import {fromEvent, timer} from 'rxjs';
+import {filter, scan, shareReplay, take, takeUntil} from "rxjs/operators";
 
 const countDownNumber = 10;
 const speed = 1000;
@@ -7,22 +7,26 @@ const speed = 1000;
 const scanFunc = (x, y) => {
     return countDownNumber - y
 }
-const subFunc = (msg, next= true, complete = true) => {
+const subFunc = (msg, next = true, complete = true) => {
     return {
-        next: value => next ? console.log(value): null,
-        complete: () => complete?  console.log('completed: ' + msg) : null
+        next: value => next ? console.log(value) : null,
+        complete: () => complete ? console.log('completed: ' + msg) : null
     }
 }
+// click stream
+const click$ = fromEvent(document, 'click');
 
 const source$ = timer(0, speed).pipe(
     take(countDownNumber + 1),
+    takeUntil(click$),
     scan(scanFunc, countDownNumber),
     // tap(() => console.log('...')),
     shareReplay());
 
+
 const countDown$ = source$.pipe(filter(x => x > 0));
 const takeOff$ = source$.pipe(filter(x => x === 0));
-const mid$ = source$.pipe( filter(x => x === Math.round(countDownNumber / 2)),take(1));
+const mid$ = source$.pipe(filter(x => x === Math.round(countDownNumber / 2)), take(1));
 
 countDown$.subscribe(subFunc('countDown'));
 takeOff$.subscribe(subFunc('takeOff!', null));
