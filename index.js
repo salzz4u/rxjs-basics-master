b6da44acb655a1e7219edd4e8b6f837aa4e5cde5
salim kapasi <sgkapasi@gmail.com>
2021-03-18 17:26:57 -0400

    example of sample where another observable's stream  output is used to emit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-import {fromEvent} from 'rxjs';
-import {throttleTime} from "rxjs/operators";
+import {fromEvent, interval} from 'rxjs';
+import {sample, takeUntil, tap} from "rxjs/operators";
 import {subFunc} from "./helper";
 
 const source$ = fromEvent(document, 'click');
+const stop$ = fromEvent(document, 'keyup');
+const interval$ = interval(1000);
 
-source$.pipe(
-    throttleTime(3000),
+interval$.pipe(
+    tap(e=>console.log('...' + e)),
+    sample(source$),
+    takeUntil(stop$)
 ).subscribe(subFunc('clicked'))
 
